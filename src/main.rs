17e79dceaae7205ed22efa8ec26f71923ab3516b
Mohammad Javad Ghasemy <geeksesi@gmail.com>
2023-09-24 08:12:47 +0330

    feat: optimize version

--- src/main.rs
+++ src/main.rs
@@ -8,19 +8,20 @@ fn get_map(path: String) -> HashMap<u32, HashMap<u32, u32>> {
     serde_json::from_str::<HashMap<u32, HashMap<u32, u32>>>(&map).unwrap()
 }
 
-fn successors(&n: &u32) -> Vec<(u32, usize)> {
-    let map: HashMap<u32, HashMap<u32, u32>> =
-        get_map("/home/geeksesi/public_html/Castel/Castel/.maps/1.json".to_string());
+fn successors(&_n: &u32, map: HashMap<u32, u32>) -> Vec<(u32, usize)> {
     let mut successors: Vec<(u32, usize)> = Vec::new();
-    for (k, v) in map[&n].iter() {
+    for (k, v) in map.iter() {
         successors.push((*k, *v as usize));
     }
     successors
 }
 
 fn main() {
+    let map: HashMap<u32, HashMap<u32, u32>> =
+        get_map("/home/geeksesi/public_html/Castel/Castel/.maps/1.json".to_string());
+
     // print!("{:?}", map[&55]);
     static GOAL: u32 = 2111;
-    let reachables = dijkstra(&55, successors, |&p| p == GOAL);
+    let reachables = dijkstra(&55, |p: &u32| successors(p, map[p].clone()), |&p| p == GOAL);
     print!("Path to {:?} is ", reachables);
 }
